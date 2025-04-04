c652bd539f4b5421a632f7a2937585d73985a744
Sandeep Saravanakumar <sarava16@eceprog1.ecn.purdue.edu>
2025-04-04 14:15:00 -0400

    faster hw10

--- linked_list.c
+++ linked_list.c
@@ -2,22 +2,102 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value)
+{ 
+  struct list_node* node = malloc(sizeof(struct list_node));
+  node->value = value;
+  node->next = NULL;
+  return node; 
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) 
+{
+  struct list_node *node = new_node(value);
+  node->next = (list->head);
+  list->head = node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) 
+{
+  struct list_node *node = new_node(value);
+  
+  node->next = NULL;
+  if (list->head == NULL) 
+  {
+    list->head = node;
+    return;
+  }
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+  struct list_node *temp = list->head;
+  while (temp->next != NULL) 
+  {
+    temp = temp->next;
+  }
+  temp->next = node;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) 
+{ 
+  if (list->head == NULL)
+  {
+    return 34;  //something greater than 15 (max tile number)
+  }
 
-void free_list(struct linked_list list) {}
+  struct list_node *temp = list->head;  
+  size_t value = temp->value;    
+  list->head = temp->next;  
+  
+  free(temp);  
+  return value;
+}
+
+size_t remove_from_tail(struct linked_list *list) 
+{
+  struct list_node *temp = list->head;
+  size_t value = 0;
+  if(list->head == NULL)
+  {
+    return 34; //something greater than 15 (max tile number)
+  }
+  
+  if (temp->next == NULL) 
+  {
+    value = temp->value;
+    free(temp);
+    list->head = NULL; 
+    return value;
+  }
+
+  while (temp->next->next != NULL) 
+  {
+    temp = temp->next;
+  }
+
+  value = temp->next->value;
+  free(temp->next);
+  temp->next = NULL;
+
+  return value;
+}
+
+void free_list(struct linked_list list) 
+
+{
+  struct list_node* temp = list.head;
+  while (temp != NULL) 
+    {
+      struct list_node *next = temp->next;
+      free(temp);
+      temp = next;
+    }
+}
 
 // Utility function to help you debugging, do not modify
-void dump_list(FILE *fp, struct linked_list list) {
+void dump_list(FILE *fp, struct linked_list list) 
+{
   fprintf(fp, "[ ");
-  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next) {
+  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next) 
+  {
     fprintf(fp, "%zu ", cur->value);
   }
   fprintf(fp, "]\n");
--- queue.c
+++ queue.c
@@ -1,8 +1,98 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+#define VISITED_SIZE 1000003
+char visited[VISITED_SIZE] = {0};
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+void enqueue(struct queue *q, struct game_state state) 
+{
+    size_t temp = serialize(state);
+    insert_at_head(&q->data,temp);
 
-int number_of_moves(struct game_state start) { return 0; }
+}
+
+struct game_state dequeue(struct queue *q) 
+{ 
+    size_t temp = remove_from_tail(&q->data);
+    return deserialize(temp); 
+}
+
+bool match(uint8_t tiles[4][4])
+{
+    int exp = 1;
+    for(int r = 0; r<4;r++)
+    {
+        for(int c = 0; c<4; c++)
+        {
+            if(r == 3 && c == 3)
+            {
+                return (tiles[r][c] == 0);
+            }
+            if(tiles[r][c] != exp++)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int number_of_moves(struct game_state start) 
+{ 
+    struct queue q;
+    q.data.head = NULL;
+    enqueue(&q,start);
+    while (q.data.head != NULL) 
+    {
+        struct game_state cur = dequeue(&q);
+
+        if (match(cur.tiles)) 
+        {
+            return cur.num_steps;
+        }
+
+        struct game_state next;
+
+        next = cur;
+        move_up(&next);
+        uint64_t key = serialize(next);
+        size_t index = key % VISITED_SIZE;
+        if (!visited[index]) 
+        {
+            visited[index] = 1;
+            enqueue(&q, next);
+        }
+
+        next = cur;
+        move_down(&next);
+        key = serialize(next);
+        index = key % VISITED_SIZE;
+        if (!visited[index]) 
+        {
+            visited[index] = 1;
+            enqueue(&q, next);
+        }
+
+        next = cur;
+        move_left(&next);
+        key = serialize(next);
+        index = key % VISITED_SIZE;
+        if (!visited[index]) 
+        {
+            visited[index] = 1;
+            enqueue(&q, next);
+        }
+
+        next = cur;
+        move_right(&next);
+        key = serialize(next);
+        index = key % VISITED_SIZE;
+        if (!visited[index]) 
+        {
+            visited[index] = 1;
+            enqueue(&q, next);
+        }
+    }
+
+    return -1; // No solution found
+}
